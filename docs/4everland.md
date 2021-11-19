6b7c643eea43873e95c59ed275abf969ab44c96b
biveivk <90406814+biveivk@users.noreply.github.com>
2021-11-19 11:12:54 +0800

    new content

---
+++ docs/4everland.md
@@ -0,0 +1,7 @@
+---
+id: 4everland
+title: 4everland
+sidebar_label: 4everland
+slug: /4everland
+---
+[4EVERLAND](https://www.4everland.org/)是一个具有全球加速、隐私保护、分布式存储等技术特性的Web3.0云计算平台。详细内容请看[4everland官方文档](https://docs.hosting.4everland.org/guide/#_4ever-hosting)
--- sidebars.js
+++ sidebars.js
@@ -4,9 +4,11 @@ module.exports = {
       "游戏介绍": ["avatar-system", "legacy-system", "life-system", "time-system","attribute-system","occupation-system","nvwaoracle","windai"],
       游戏经济: ["economicintroduction", "nft"],
       发展路线: ["map"],
-      技术支持: ["solana","gpt-3","unrealengine","arweave"],
+      技术支持: ["solana","gpt-3","unrealengine","arweave","4everland"],
       新手指南: ["wallet","create","participate"],
+      常见问题: ["community-question","game-question"],
+      游戏灵感 :["technology","art","philosophy"]
     },
-    "faq",
+    
   ],
 };
