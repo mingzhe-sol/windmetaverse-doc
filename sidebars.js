68e051d7dbb1321460aeb8a3d519ece86f4131bd
biveivk <90406814+biveivk@users.noreply.github.com>
2021-11-29 21:22:43 +0800

    V1.11

--- sidebars.js
+++ sidebars.js
@@ -5,7 +5,7 @@ module.exports = {
       游戏经济: ["economicintroduction", "nft"],
       发展路线: ["map"],
       技术支持: ["solana","gpt-3","unrealengine","arweave","othertec"],
-      新手指南: ["wallet","create","participate"],
+      新手指南: ["wallet","participate","create"],
       游戏灵感: ["technology","art","philosophy"],
       常见问题: ["community-question","game-question"]
       
